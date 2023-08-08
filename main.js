ec7971e12356076134b3e41d1b264282a384fa25
AdakoleDest <ochelxy@gmail.com>
2023-08-08 19:15:26 +0100

    Added a side menu bar

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBtn = document.querySelector(".mobile-nav-btn");
 const categoriesMobile = document.querySelector(".categories-mobile");
+const cancelBtn = document.querySelector(".cancel-btn");
 
 navBtn.onclick = function(){
     categoriesMobile.style.display = "block";
-    categoriesMobile.style.position = "absolute";
-    categoriesMobile.style.position = "absolute";
+    navBtn.style.display = "none";
+    cancelBtn.style.display = "block";
 }
+cancelBtn.onclick = function(){
+    categoriesMobile.style.display = "none";
+    navBtn.style.display = "block";
+    cancelBtn.style.display = "none";
+}
+
