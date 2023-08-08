cad11de57749063aca48db3da3ebdf8442ecb0e0
AdakoleDest <ochelxy@gmail.com>
2023-08-08 12:19:28 +0100

    re-modified

---
+++ main.js
@@ -0,0 +1,8 @@
+const navBtn = document.querySelector(".mobile-nav-btn");
+const categoriesMobile = document.querySelector(".categories-mobile");
+
+navBtn.onclick = function(){
+    categoriesMobile.style.display = "block";
+    categoriesMobile.style.position = "absolute";
+    categoriesMobile.style.position = "absolute";
+}
