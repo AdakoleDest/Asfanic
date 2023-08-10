e28c058c66f85de445ec843f8e0c1056f1be142e
AdakoleDest <ochelxy@gmail.com>
2023-08-10 20:36:35 +0100

    Added focus functionality to the menu bar elements

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const categoriesMobile = document.querySelector(".categories-mobile");
 const cancelBtn = document.querySelector(".cancel-btn");
 
 navBtn.onclick = function(){
+   
     categoriesMobile.style.display = "block";
     navBtn.style.display = "none";
     cancelBtn.style.display = "block";
